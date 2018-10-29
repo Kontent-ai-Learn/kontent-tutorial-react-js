ec31b5fac7b13eee62c65d0314a4cbaec34f91b3
Juraj Uhlar <JurajU@kentico.com>
2018-10-29 16:34:51 +0100

    clean up

--- src/ArticleView.js
+++ src/ArticleView.js
@@ -11,28 +11,13 @@ const deliveryClient = new DeliveryClient({ projectId: 'a0a9d198-e604-007a-50c9-
 class ArticleView extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       loaded: false,
       article: null,
     };
   }
 
-  componentDidMount() {
-    let slug = this.props.match.params.slug;
-    console.log(slug);
-    this.getArticle(slug);
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    let oldSlug = this.props.match.params.slug;
-    let newSlug = nextProps.match.params.slug;
-    if (oldSlug !== newSlug) {
-      this.getArticle(newSlug);
-    }
-  }
-
-  getArticle(slug) {
+  fethcArticle(slug) {
     deliveryClient.item(slug)
       .depthParameter(2)
       .queryConfig({
@@ -49,16 +34,30 @@ class ArticleView extends Component {
       });
   }
 
-  handleClick(e, richTextElement) {
-    if (e.target.tagName === 'A' && e.target.hasAttribute('data-item-id')) {
-      e.preventDefault();
+  handleClick(event, richTextElement) {
+    if (event.target.tagName === 'A' && event.target.hasAttribute('data-item-id')) {
+      event.preventDefault();
 
-      const id = e.target.getAttribute('data-item-id');
+      const id = event.target.getAttribute('data-item-id');
       const link = richTextElement.links.find(link => link.itemId === id);
       const newPath = resolveContentLink(link);
       if (newPath) {
         this.props.history.push(newPath);
       }
+    }
+  }
+
+  componentDidMount() {
+    let slug = this.props.match.params.slug;
+    console.log(slug);
+    this.fethcArticle(slug);
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    let oldSlug = this.props.match.params.slug;
+    let newSlug = nextProps.match.params.slug;
+    if (oldSlug !== newSlug) {
+      this.fethcArticle(newSlug);
     }
   }
 
@@ -74,7 +73,7 @@ class ArticleView extends Component {
           <h1>{title}</h1>
           <div
             dangerouslySetInnerHTML={{ __html: bodyCopy.getHtml() }}
-            onClick={e => this.handleClick(e, bodyCopy)} />
+            onClick={event => this.handleClick(event, bodyCopy)} />
         </div>
       );
     } else {
