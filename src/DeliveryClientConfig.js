1e0a51586457a41c39b89eecdc92fb331a42d074
Juraj Uhlar <JurajU@kentico.com>
2018-12-03 13:45:22 +0100

    Update SDK version

--- src/DeliveryClientConfig.js
+++ src/DeliveryClientConfig.js
@@ -1,3 +1,8 @@
 import { DeliveryClient } from 'kentico-cloud-delivery';
 
-export const deliveryClient = new DeliveryClient({ projectId: 'a0a9d198-e604-007a-50c9-fecbb46046d1' });
+export const deliveryClient = new DeliveryClient({ 
+  projectId: 'a0a9d198-e604-007a-50c9-fecbb46046d1',
+  // projectId: '975bf280-fd91-488c-994c-2f04416e5ee3',
+  enableAdvancedLogging: false,
+  });
+
